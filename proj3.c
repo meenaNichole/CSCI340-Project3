e9c8f7590fd1e346190aaecaf581aa19065d6c59
Christian Ellwood <38473927+Jellwood@users.noreply.github.com>
2020-03-30 14:30:33 -0400

    Producer Queue started
    Producer Queue is initialized and running the function, however, it errors out due to stack smashing detected, likely something with moving things into the string buffers.

--- proj3.c
+++ proj3.c
@@ -5,9 +5,12 @@
 #include <semaphore.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
-
+//Global variable for word count, just to avoid carrying around an extra variable.
 int totalWordCount = 0;
+//Global semaphore for updating the variable
+sem_t update;
 //Christian, you can change any variable names if you don't like them or feel like they don't make sense.
 
 //I wanted to name this struct 'entry' but I figured we should go about this like a linked list of sorts...if that makes sense
@@ -19,8 +22,10 @@ struct node {
 
 //I don't think we actually need this, but I don't know how else to do the pop function.
 typedef struct {
-	sem_t *empty;
-	sem_t *full;
+	int q_len;
+	char* buffer;
+	sem_t empty;
+	sem_t full;
 	pthread_mutex_t *lock;
 	struct node* head;
 	struct node* tail;
@@ -28,8 +33,14 @@ typedef struct {
 
 struct threadData {
 	char* currLine;
-	sem_t* updating;
+	sem_t updating;
 };
+
+struct producerData {
+	Queue* quOut;
+	FILE* textFile;
+};
+
 /*
 struct node *head = NULL;
 struct node *tail = NULL;
@@ -42,7 +53,7 @@ pthread_mutex_t lock;
 char* pop(Queue* queue){
 	struct node *currLine;
 
-	assert(sem_wait(queue->full) == 0);
+	assert(sem_wait(&queue->full) == 0);
 	assert(pthread_mutex_lock(queue->lock) == 0);
 
 	currLine = queue->head->next;
@@ -51,7 +62,7 @@ char* pop(Queue* queue){
 	currLine->next = currLine->prev = NULL;
 	
 	assert(pthread_mutex_unlock(queue->lock) == 0);	
-	assert(sem_post(queue->empty) == 0);
+	assert(sem_post(&queue->empty) == 0);
 
 	return currLine->line;
 
@@ -60,39 +71,44 @@ char* pop(Queue* queue){
 void append(Queue* queue, char* line){
 	struct node *newLine = malloc(sizeof(struct node));
 	strcpy(line, newLine->line);
-	assert(sem_post(queue->full) == 0);
+	assert(sem_post(&queue->full) == 0);
 	assert(pthread_mutex_lock(queue->lock) == 0);
 	newLine->prev = queue->tail->prev;
 	newLine->next = queue->tail;
 	queue->tail->prev->next = newLine;
 	queue->tail->prev = newLine;
 	assert(pthread_mutex_unlock(queue->lock) == 0);
-	assert(sem_wait(queue->empty) == 0);
+	assert(sem_wait(&queue->empty) == 0);
 }
 
+void producerFn(void* args) {
+	struct producerData* prodData = (struct producerData*) args;
+	Queue* lines = prodData->quOut;
+	FILE* textFile = prodData->textFile;
+	char* line;
+	while (!feof(textFile)) {
+		fgets(line, 1024, textFile);
+		append(lines, line);
+	}
+	return NULL;
+}
 
 //Prints out the number of words in a line, might need to change this later to not include white spaces, haven't decided yet
 //Honestly, this probably will definitely need to be changed. probably maybe idk
 
-int wordCount(void *threaddata){
-	struct threadData* wordCount = (struct threadData *) threaddata;
+int wordCount(char* line){
 	int numOfWords = 0;
 	int i = 0;
-	char* currLine = wordCount->currLine;
-	sem_t* update = wordCount->updating;
+	char* currLine = line;
 	for (i = 0; currLine[i]; i++){
 		if (currLine[i] == 32){
 			numOfWords++;		
 		}
 	}
 
-	if (i > 0){
-		numOfWords++;
-	}
-
-	sem_wait(update);
+	sem_wait(&update);
 	totalWordCount += numOfWords;
-	sem_post(update);
+	sem_post(&update);
 	
 	return 0;
 }
@@ -100,32 +116,42 @@ int wordCount(void *threaddata){
 
 int main(int argc, char *argv[]){
 	
+	
 	//number of tasks to run
-	int maxLength = 128;
+	int maxLength = 1024;
 	int tasks = atoi(argv[1]);
-	pthread_t listOfThreads[sizeof(pthread_t) * tasks];
 	int numLines = 0;
 	int numWords = 0;
-	FILE* textFile;
-	textFile = fopen("midsummer.txt", "r");
 	char* line[maxLength];
-	sem_t update;
+
+	pthread_t producerThread;
+	pthread_t listOfThreads[sizeof(pthread_t) * tasks];
+
+	FILE* textFile;
+	Queue lines;
+
 	assert(sem_init(&update, 0, 1) == 0);
+	lines.q_len = 10;
+	lines.buffer = malloc(maxLength);
+	sem_init(&lines.empty, 0, lines.q_len);
+	sem_init(&lines.full, 0, 0);
+	pthread_mutex_init(&lines.lock, NULL);
+	lines.head = lines.tail = NULL;
+
+	textFile = fopen("./midsummer.txt", "r");
+	struct producerData prodData = {&lines, textFile};
+	assert(sem_init(&update, 0, 1) == 0);
+	assert(pthread_create(&producerThread, NULL, producerFn, (void*)&prodData) == 0);
 
 	//TEST LINE
-	printf("%d tasks total", tasks);
+	printf("%d tasks total\n", tasks);
 	//Scans until end of file (ideally) fgets runs until it encounters EOF so spaces should be fine
-	while (fgets(*line, 128, textFile) != NULL) {
-		struct threadData* threaddata = malloc(sizeof(struct threadData));
-		threaddata->currLine = *line;
-		threaddata->updating = update;
-		for(int i = 0; i < tasks; i++) {
-		
-			assert(pthread_create(&listOfThreads[i], NULL, (void*)wordCount, (void*)threaddata) == 0);
-			
-		}
+/*
+	for(int i = 0; i < sizeof(listOfThreads) / sizeof(pthread_t*); i++) {
+		assert(pthread_join(listOfThreads[i], NULL) == 0);
 	}
+*/
 
-	printf("Number of words: %d", totalWordCount);
+	printf("Number of words: %d\n", totalWordCount);
 
 }
