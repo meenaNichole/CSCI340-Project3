16d8cf72ba58f197fc00810dd9eb5ff57021aefb
Christian Ellwood <38473927+Jellwood@users.noreply.github.com>
2020-04-01 17:33:31 -0400

    Producer Queue Initialized
    Queue is initialized, just not producing lines

--- proj3.c
+++ proj3.c
@@ -26,7 +26,7 @@ typedef struct {
 	char* buffer;
 	sem_t empty;
 	sem_t full;
-	pthread_mutex_t *lock;
+	pthread_mutex_t lock;
 	struct node* head;
 	struct node* tail;
 }Queue;
@@ -54,43 +54,48 @@ char* pop(Queue* queue){
 	struct node *currLine;
 
 	assert(sem_wait(&queue->full) == 0);
-	assert(pthread_mutex_lock(queue->lock) == 0);
+	assert(pthread_mutex_lock(&queue->lock) == 0);
 
 	currLine = queue->head->next;
 	queue->head->next = currLine->next;
 	currLine->next->prev = queue->head;
 	currLine->next = currLine->prev = NULL;
 	
-	assert(pthread_mutex_unlock(queue->lock) == 0);	
+	assert(pthread_mutex_unlock(&queue->lock) == 0);	
 	assert(sem_post(&queue->empty) == 0);
 
 	return currLine->line;
 
 }
 
-void append(Queue* queue, char* line){
+void append(Queue* queue, char* inLine){
+	printf("Starting Append");
 	struct node *newLine = malloc(sizeof(struct node));
-	strcpy(line, newLine->line);
-	assert(sem_post(&queue->full) == 0);
-	assert(pthread_mutex_lock(queue->lock) == 0);
+	newLine->line[1024];
+	printf("%lu", strlen(inLine));
+	//strncpy(inLine, newLine->line, 1024);
+	assert(sem_wait(&queue->empty) == 0);
+	assert(pthread_mutex_lock(&queue->lock) == 0);
 	newLine->prev = queue->tail->prev;
 	newLine->next = queue->tail;
 	queue->tail->prev->next = newLine;
 	queue->tail->prev = newLine;
-	assert(pthread_mutex_unlock(queue->lock) == 0);
-	assert(sem_wait(&queue->empty) == 0);
+	assert(pthread_mutex_unlock(&queue->lock) == 0);
+	assert(sem_post(&queue->full) == 0);
+	printf("Ending Append");
 }
 
 void producerFn(void* args) {
+	printf("Starting Producer Fn");
 	struct producerData* prodData = (struct producerData*) args;
 	Queue* lines = prodData->quOut;
 	FILE* textFile = prodData->textFile;
-	char* line;
-	while (!feof(textFile)) {
-		fgets(line, 1024, textFile);
+	char line[1024];
+	while (fgets(line, 1024, textFile)) {
 		append(lines, line);
+		printf("%s", line);
 	}
-	return NULL;
+	printf("Ending Producer Fn");
 }
 
 //Prints out the number of words in a line, might need to change this later to not include white spaces, haven't decided yet
@@ -113,6 +118,15 @@ int wordCount(char* line){
 	return 0;
 }
 
+void list_init(Queue* quInit) {
+	quInit->q_len = 5;
+	quInit->buffer[1024];
+	sem_init(&quInit->empty, 0, quInit->q_len);
+	sem_init(&quInit->full, 0, 0);
+	pthread_mutex_init(&quInit->lock, NULL);
+	quInit->head = quInit->tail = NULL;
+}
+
 
 int main(int argc, char *argv[]){
 	
@@ -130,19 +144,16 @@ int main(int argc, char *argv[]){
 	FILE* textFile;
 	Queue lines;
 
+	list_init(&lines);
+
 	assert(sem_init(&update, 0, 1) == 0);
-	lines.q_len = 10;
-	lines.buffer = malloc(maxLength);
-	sem_init(&lines.empty, 0, lines.q_len);
-	sem_init(&lines.full, 0, 0);
-	pthread_mutex_init(&lines.lock, NULL);
-	lines.head = lines.tail = NULL;
 
 	textFile = fopen("./midsummer.txt", "r");
 	struct producerData prodData = {&lines, textFile};
 	assert(sem_init(&update, 0, 1) == 0);
-	assert(pthread_create(&producerThread, NULL, producerFn, (void*)&prodData) == 0);
+	assert(pthread_create(&producerThread, NULL, (void*)producerFn, (void*)&prodData) == 0);
 
+	pthread_join(producerThread, NULL);
 	//TEST LINE
 	printf("%d tasks total\n", tasks);
 	//Scans until end of file (ideally) fgets runs until it encounters EOF so spaces should be fine
@@ -151,7 +162,6 @@ int main(int argc, char *argv[]){
 		assert(pthread_join(listOfThreads[i], NULL) == 0);
 	}
 */
-
 	printf("Number of words: %d\n", totalWordCount);
 
 }
